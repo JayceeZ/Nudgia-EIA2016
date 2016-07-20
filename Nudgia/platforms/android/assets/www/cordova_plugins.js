f44f009d453c747570fee15285edf0b78491c23d
Jean-Christophe Isoard <jeanchristophe.isoard@gmail.com>
2016-07-20 18:34:05 +0200

    Lets forget about minimal git as it will slow us down

---
+++ Nudgia/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,75 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+        "id": "cordova-plugin-geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "id": "cordova-plugin-geolocation.PositionError",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-camerapicturebackground/www/CameraPictureBackground.js",
+        "id": "cordova-plugin-camerapicturebackground.CameraPictureBackground",
+        "clobbers": [
+            "window.plugins.CameraPictureBackground"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-background-mode/www/background-mode.js",
+        "id": "cordova-plugin-background-mode.BackgroundMode",
+        "clobbers": [
+            "cordova.plugins.backgroundMode",
+            "plugin.backgroundMode"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification",
+        "clobbers": [
+            "cordova.plugins.notification.local",
+            "plugin.notification.local"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-core.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Core",
+        "clobbers": [
+            "cordova.plugins.notification.local.core",
+            "plugin.notification.local.core"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-util.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Util",
+        "merges": [
+            "cordova.plugins.notification.local.core",
+            "plugin.notification.local.core"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-geolocation": "2.2.0",
+    "cordova-plugin-camerapicturebackground": "0.0.3",
+    "cordova-plugin-background-mode": "0.6.5",
+    "de.appplant.cordova.plugin.local-notification": "0.8.4",
+    "cordova-plugin-compat": "1.0.0",
+    "cordova-plugin-device": "1.1.2",
+    "cordova-plugin-app-event": "1.2.0"
+}
+// BOTTOM OF METADATA
+});
---
+++ Nudgia/platforms/android/assets/www/js/index.js
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+  TIMER_VALUE: 1000,
+  deviceready: true,
+  // Application Constructor
+  initialize: function () {
+    this.bindEvents();
+    setInterval(app.timedThings, app.TIMER_VALUE);
+  },
+
+  bindEvents: function () {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+
+  onDeviceReady: function () {
+    app.receivedEvent('deviceready');
+    app.deviceready = true;
+    geolocation.watchLocation();
+  },
+
+  // Update DOM on a Received Event
+  receivedEvent: function (id) {
+    var thresholdSliderDOM = document.getElementById("threshold");
+    thresholdSliderDOM.addEventListener('change', picture.onThresholdChange);
+    app.enableBackground();
+  },
+
+  enableBackground: function() {
+    cordova.plugins.backgroundMode.setDefaults({
+      title: "Nudgia is watching you !",
+      text: "You can display the app by clicking me"
+    });
+    cordova.plugins.backgroundMode.enable();
+    if(cordova.plugins.backgroundMode.isEnabled()) {
+      app.logDebug("Background mode enabled");
+    }
+  },
+
+  __alert: function (message) {
+    this.logDebug(message);
+    alert(message);
+  },
+
+  logDebug: function (message) {
+    var debugDOM = document.getElementById("debug");
+    debugDOM.innerHTML = message + "\n" + debugDOM.innerHTML;
+  },
+
+  logError: function (message) {
+    this.__alert(message, true);
+  }
+};
+
+app.initialize();
